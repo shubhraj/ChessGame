2615272e0d33e9bc4b78f0d1d7ed256c52e7f03c
dullus <Qwerty@12345>
2016-07-30 14:36:32 +0530

    Modified

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,9 +1,25 @@
 /**
  * Created by dullus on 7/30/2016.
  */
-public class Bishop {
 
-    Bishop(){
+
+    public class Bishop extends Piece{
+
+        public Bishop(boolean available, int x, int y) {
+            super(available, x, y);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
+            if(super.isValid(board, fromX, fromY, toX, toY) == false)
+                return false;
+
+            if(toX - fromX == toY - fromY)
+                return true;
+
+            return false;
+        }
 
     }
-}
+
